ca36b5e7bb2b1c01426430261a8b2b9acabab4cb
sadhamhussain46 <sadhamhussain.alpharive@gmail.com>
2023-01-27 19:26:12 +0530

    Send and deposit design completed

--- lib/screens/home/send_page.dart
+++ lib/screens/home/send_page.dart
@@ -70,248 +70,255 @@ class _SendScreenState extends State<SendScreen> {
   }
 
   sendUI(){
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Form(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Send"+widget.coinDetails!.name.toString(),
-                style: CustomWidget(context: context)
-                    .CustomSizedTextStyle(
-                    18.0,
-                    AppColors.blackColor,
-                    FontWeight.w600,
-                    'FontRegular'),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Text(
-                "Send"+ widget.coinDetails!.coin.toString()+" to crypto address ",
-                style: CustomWidget(context: context)
-                    .CustomSizedTextStyle(
-                    14.0,
-                    Color(0xFFbdbdbd),
-                    FontWeight.w500,
-                    'FontRegular'),
-              ),
-              SizedBox(
-                height: 45.0,
-              ),
-              Text(
-                "Address ",
-                style: CustomWidget(context: context)
-                    .CustomSizedTextStyle(
-                    14.0,
-                    AppColors.blackColor,
-                    FontWeight.w600,
-                    'FontRegular'),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              TextFormFieldCustom(
-                onEditComplete: () {
-                  addressFocus.unfocus();
-                  FocusScope.of(context).requestFocus(addressFocus);
-                },
-                radius: 15.0,
-                error: "Enter the address",
-                textColor: AppColors.blackColor,
-                borderColor: AppColors.hintColor.withOpacity(0.2),
-                fillColor: AppColors.whiteColor,
-                hintStyle: CustomWidget(context: context).CustomSizedTextStyle(
-                    15.0, AppColors.hintColor, FontWeight.w500, 'FontRegular'),
-                textStyle: CustomWidget(context: context).CustomTextStyle(
-                    AppColors.blackColor, FontWeight.w500, 'FontRegular'),
-                textInputAction: TextInputAction.next,
-                focusNode: addressFocus,
-                maxlines: 1,
-                text: '',
-                hintText: "Long press and paste,Address,ENS etc..",
-                obscureText: false,
-                suffix: Container(
-                  padding: EdgeInsets.only(right: 10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Icon(Icons.qr_code_2,color: Colors.black.withOpacity(0.7),),
-                      Text("Paste".toUpperCase(),style: TextStyle(fontSize:15.0,fontWeight:FontWeight.w500,color:  Color(0xFFbdbdbd),fontFamily: 'FontRegular'),)
-                    ],
-                  ),
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Form(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Send"+widget.coinDetails!.name.toString(),
+                  style: CustomWidget(context: context)
+                      .CustomSizedTextStyle(
+                      18.0,
+                      AppColors.blackColor,
+                      FontWeight.w600,
+                      'FontRegular'),
                 ),
-                textChanged: (value) {},
-                onChanged: () {},
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Please enter address";
-                  }
-                  return null;
-                },
-                enabled: true,
-                textInputType: TextInputType.name,
-                controller: addressController,
-              ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  "Send"+ widget.coinDetails!.coin.toString()+" to crypto address ",
+                  style: CustomWidget(context: context)
+                      .CustomSizedTextStyle(
+                      14.0,
+                      Color(0xFFbdbdbd),
+                      FontWeight.w500,
+                      'FontRegular'),
+                ),
+                SizedBox(
+                  height: 45.0,
+                ),
+                Text(
+                  "Address ",
+                  style: CustomWidget(context: context)
+                      .CustomSizedTextStyle(
+                      14.0,
+                      AppColors.blackColor,
+                      FontWeight.w600,
+                      'FontRegular'),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                TextFormFieldCustom(
+                  onEditComplete: () {
+                    addressFocus.unfocus();
+                    FocusScope.of(context).requestFocus(addressFocus);
+                  },
+                  radius: 15.0,
+                  error: "Enter the address",
+                  textColor: AppColors.blackColor,
+                  borderColor: AppColors.hintColor.withOpacity(0.2),
+                  fillColor: AppColors.whiteColor,
+                  hintStyle: CustomWidget(context: context).CustomSizedTextStyle(
+                      15.0, AppColors.hintColor, FontWeight.w500, 'FontRegular'),
+                  textStyle: CustomWidget(context: context).CustomTextStyle(
+                      AppColors.blackColor, FontWeight.w500, 'FontRegular'),
+                  textInputAction: TextInputAction.next,
+                  focusNode: addressFocus,
+                  maxlines: 1,
+                  text: '',
+                  hintText: "Long press and paste,Address,ENS etc..",
+                  obscureText: false,
+                  suffix:Padding(
+                    padding: EdgeInsets.only(right: 10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min, //
+                      children: [
+                        Icon(Icons.qr_code_2,color: Colors.black.withOpacity(0.7),),
+                        SizedBox(width: 3.0,),
+                        Text("Paste".toUpperCase(),style: TextStyle(fontSize:15.0,fontWeight:FontWeight.w500,color:  Color(0xFFbdbdbd),fontFamily: 'FontRegular'),)
+                      ],
+                    ),
+                  ),
+                  textChanged: (value) {},
+                  onChanged: () {},
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please enter address";
+                    }
+                    return null;
+                  },
+                  enabled: true,
+                  textInputType: TextInputType.name,
+                  controller: addressController,
+                ),
 
-              SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                "Network",
-                style: CustomWidget(context: context)
-                    .CustomSizedTextStyle(
-                    14.0,
-                    AppColors.blackColor,
-                    FontWeight.w600,
-                    'FontRegular'),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: AppColors.hintColor.withOpacity(0.3)),
-                  borderRadius: BorderRadius.circular(20.0),
+                SizedBox(
+                  height: 20.0,
                 ),
-                padding: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
-                child: DropdownButton(
-                  underline: Container(
-                    height: 0.0,
+                Text(
+                  "Network",
+                  style: CustomWidget(context: context)
+                      .CustomSizedTextStyle(
+                      14.0,
+                      AppColors.blackColor,
+                      FontWeight.w600,
+                      'FontRegular'),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: AppColors.hintColor.withOpacity(0.3)),
+                    borderRadius: BorderRadius.circular(20.0),
                   ),
-                  menuMaxHeight: MediaQuery.of(context).size.height * 0.7,
-                  items: _selectedNetwork
-                      .map((value) => DropdownMenuItem(
-                    child: Text(
-                      value.toString(),
-                      style: CustomWidget(context: context)
-                          .CustomSizedTextStyle(
-                          14.0,
-                          AppColors.blackColor,
+                  padding: EdgeInsets.fromLTRB(10.0, 3.0, 10.0, 3.0),
+                  child: DropdownButton(
+                    underline: Container(
+                      height: 0.0,
+                    ),
+                    menuMaxHeight: MediaQuery.of(context).size.height * 0.5,
+                    items: _selectedNetwork
+                        .map((value) => DropdownMenuItem(
+                      child: Text(
+                        value.toString(),
+                        style: CustomWidget(context: context)
+                            .CustomSizedTextStyle(
+                            14.0,
+                            AppColors.blackColor,
+                            FontWeight.w500,
+                            'FontRegular'),
+                      ),
+                      value: value,
+                    ))
+                        .toList(),
+                    onChanged: (value) async {
+                      setState(() {
+                        selectedValue=value.toString();
+                      });
+                    },
+                    hint: Text(
+                      "Select correct network",
+                      style: CustomWidget(context: context).CustomSizedTextStyle(
+                          12.0,
+                          Theme.of(context).errorColor,
                           FontWeight.w500,
                           'FontRegular'),
                     ),
-                    value: value,
-                  ))
-                      .toList(),
-                  onChanged: (value) async {
-                    setState(() {
-                      selectedValue=value.toString();
-
-                    });
+                    isExpanded: true,
+                    value: selectedValue,
+                    icon: const Icon(
+                      Icons.arrow_drop_down,
+                      // color: AppColors.otherTextColor,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 25.0,
+                ),
+                Text(
+                  "Amount ",
+                  style: CustomWidget(context: context)
+                      .CustomSizedTextStyle(
+                      14.0,
+                      AppColors.blackColor,
+                      FontWeight.w600,
+                      'FontRegular'),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                TextFormFieldCustom(
+                  onEditComplete: () {
+                    amountFocus.unfocus();
+                    FocusScope.of(context).requestFocus(addressFocus);
                   },
-                  hint: Text(
-                    "Select correct network",
-                    style: CustomWidget(context: context).CustomSizedTextStyle(
-                        12.0,
-                        Theme.of(context).errorColor,
-                        FontWeight.w500,
-                        'FontRegular'),
+                  radius: 15.0,
+                  error: "Enter the amount",
+                  textColor: AppColors.blackColor,
+                  borderColor: AppColors.hintColor.withOpacity(0.2),
+                  fillColor: AppColors.whiteColor,
+                  hintStyle: CustomWidget(context: context).CustomSizedTextStyle(
+                      15.0, AppColors.hintColor, FontWeight.w500, 'FontRegular'),
+                  textStyle: CustomWidget(context: context).CustomTextStyle(
+                      AppColors.blackColor, FontWeight.w500, 'FontRegular'),
+                  textInputAction: TextInputAction.done,
+                  focusNode: amountFocus,
+                  maxlines: 1,
+                  text: '',
+                  hintText: " ",
+                  obscureText: false,
+                  suffix: Padding(
+                    padding: EdgeInsets.only(right: 10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min, //
+                      children: [
+                        Text("max ".toUpperCase(),style: TextStyle(fontSize:15.0,fontWeight:FontWeight.w500,color:  Color(0xFFbdbdbd),fontFamily: 'FontRegular'),),
+                        Text("ETH".toUpperCase(),style: TextStyle(fontSize:15.0,fontWeight:FontWeight.w500,color:  Color(0xFFbdbdbd),fontFamily: 'FontRegular'),)
+                      ],
+                    ),
                   ),
-                  isExpanded: true,
-                  value: selectedValue,
-                  icon: const Icon(
-                    Icons.arrow_drop_down,
-                    // color: AppColors.otherTextColor,
-                  ),
+                  textChanged: (value) {},
+                  onChanged: () {},
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please enter address";
+                    }
+                    return null;
+                  },
+                  enabled: true,
+                  textInputType: TextInputType.number,
+                  controller: amountController,
                 ),
-              ),
-              SizedBox(
-                height: 25.0,
-              ),
-              Text(
-                "Amount ",
-                style: CustomWidget(context: context)
-                    .CustomSizedTextStyle(
-                    14.0,
-                    AppColors.blackColor,
-                    FontWeight.w600,
-                    'FontRegular'),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              TextFormFieldCustom(
-                onEditComplete: () {
-                  amountFocus.unfocus();
-                  FocusScope.of(context).requestFocus(addressFocus);
-                },
-                radius: 15.0,
-                error: "Enter the amount",
-                textColor: AppColors.blackColor,
-                borderColor: AppColors.hintColor.withOpacity(0.2),
-                fillColor: AppColors.whiteColor,
-                hintStyle: CustomWidget(context: context).CustomSizedTextStyle(
-                    15.0, AppColors.hintColor, FontWeight.w500, 'FontRegular'),
-                textStyle: CustomWidget(context: context).CustomTextStyle(
-                    AppColors.blackColor, FontWeight.w500, 'FontRegular'),
-                textInputAction: TextInputAction.next,
-                focusNode: addressFocus,
-                maxlines: 1,
-                text: '',
-                hintText: " ",
-                obscureText: false,
-                suffix: Container(
-                  padding: EdgeInsets.only(right: 10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text("max ".toUpperCase(),style: TextStyle(fontSize:15.0,fontWeight:FontWeight.w500,color:  Color(0xFFbdbdbd),fontFamily: 'FontRegular'),),
-                      Text("ETH".toUpperCase(),style: TextStyle(fontSize:15.0,fontWeight:FontWeight.w500,color:  Color(0xFFbdbdbd),fontFamily: 'FontRegular'),)
-                    ],
-                  ),
-                ),
-                textChanged: (value) {},
-                onChanged: () {},
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Please enter address";
-                  }
-                  return null;
-                },
-                enabled: true,
-                textInputType: TextInputType.name,
-                controller: amountController,
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-        InkWell(
-          onTap: (){
-            setState(() {
+          SizedBox(
+            height: 25.0,
+          ),
+          InkWell(
+            onTap: (){
+              setState(() {
 
-              Navigator.of(context).push(
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          Transfer()));
-            });
-          },
-          child: Container(
-            padding: EdgeInsets.only(top: 15.0,bottom: 15.0),
-            decoration: BoxDecoration(
-              color: AppColors.appColor,
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            child: Center(
-              child: Text(
-                AppLocalizations.instance.text("loc_continue"),
-                style: CustomWidget(context: context)
-                    .CustomSizedTextStyle(
-                    16.0,
-                    AppColors.blackColor,
-                    FontWeight.w600,
-                    'FontRegular'),
+                Navigator.of(context).push(
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            Transfer()));
+              });
+            },
+            child: Container(
+              padding: EdgeInsets.only(top: 15.0,bottom: 15.0),
+              decoration: BoxDecoration(
+                color: AppColors.appColor,
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: Center(
+                child: Text(
+                  AppLocalizations.instance.text("loc_continue"),
+                  style: CustomWidget(context: context)
+                      .CustomSizedTextStyle(
+                      16.0,
+                      AppColors.blackColor,
+                      FontWeight.w600,
+                      'FontRegular'),
+                ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
